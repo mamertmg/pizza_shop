e85f3afb08b309f8a4bc5b9227bf167de798a37f
mamertmg <marquezgarcia.a@gmail.com>
2023-08-20 20:09:59 +0200

    added modal to pizza

--- client/src/app/components/Pizza.js
+++ client/src/app/components/Pizza.js
@@ -8,35 +8,82 @@ import PizzaDetails from "./PizzaDetails";
 
 import {IoCloseOutline} from 'react-icons/io5'
 
+// bind modal to body
+Modal.setAppElement('body');
+
+// modal styles
+const modalStyles = {
+  overlay:{
+    backgroundColor: 'rgba(0,0,0,0.5)'
+  }
+}
+
 const Pizza = ({pizza}) => {
-  return <div className="group py-2 px-4 xl:py-4 xl:px-2 rounded-xl">
 
-    {/* picture */}
-    <Image
-      width={270}
-      height={270}
-      src={pizza.image}
-      alt=''
-      priority={1}
-    />
+  const [modal, setModal] = useState(false)
 
-    {/* name */}
-    <div>
-      <div className="text-xl font-bold mb-3 capitalize cursor-pointer">
-        {pizza.name}
+  const openModal = () => {
+    setModal(true)
+  }
+
+  const closeModal = () => {
+    setModal(false)
+  }
+
+  return (
+  
+    <div className="group py-2 px-4 xl:py-4 xl:px-2 rounded-xl">
+
+      {/* picture */}
+      <Image
+        className="lg:group-hover:translate-y-3 transition-all duration-300 mb-8 cursor-pointer"
+        width={270}
+        height={270}
+        src={pizza.image}
+        alt=''
+        priority={1}
+      />
+
+      {/* name */}
+      <div>
+        <div className="text-xl font-bold mb-3 capitalize cursor-pointer">
+          {pizza.name}
+        </div>
       </div>
+
+      {/* description */}
+        <div className="text-sm font-medium min-h-[60px] mb-6">{pizza.description}</div>
+
+      {/* price */}
+      <div className="mb-6 flex items-center justify-between">
+        <div className="hidden lg:flex text-txl font-semibold">starts at {pizza.priceSm}</div>
+        <button
+          onClick={openModal} 
+          className="hidden lg:flex gradient text-white rounded-lg btn-sm font-semibold text-sm"
+        >
+          Choose
+        </button>
+        <button 
+          onClick={openModal} 
+          className="btn btn-sm gradient lg:hidden px-3 text-sm "
+        >
+          starts at {pizza.priceSm}
+        </button>
+      </div>
+
+      {/* modal */}
+      {modal && (
+        <Modal 
+          isOpen={modal} 
+          style={modalStyles} 
+          onRequestClose={closeModal}
+          contentLabel="Pizza Modal"
+        >
+          modal
+        </Modal>
+      )}
     </div>
-
-    {/* description */}
-      <div className="text-sm font-medium min-h-[60px] mb-6">{pizza.description}</div>
-
-    {/* price */}
-    <div>
-      <div className="hidden lg:flex text-txl font-semibold">starts at {pizza.priceSm}</div>
-      <button className="hidden lg:flex ">Choose</button>
-    </div>
-
-  </div>;
+  )
 };
 
 export default Pizza;
