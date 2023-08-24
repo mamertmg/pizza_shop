6cdbddd6b546fc9c1f628048fd8329563b57ff55
mamertmg <marquezgarcia.a@gmail.com>
2023-08-24 18:22:40 +0200

    added pizza details

--- client/src/app/components/Pizza.js
+++ client/src/app/components/Pizza.js
@@ -78,8 +78,15 @@ const Pizza = ({pizza}) => {
           style={modalStyles} 
           onRequestClose={closeModal}
           contentLabel="Pizza Modal"
+          className='bg-white w-full h-full lg:max-w-[900px] lg:max-h-[600px] lg:rounded-[30px] lg:fixed lg:top-[50%] lg:left-[50%] lg:translate-x-[-50%] lg:translate-y-[-50%] outline-none'
         >
-          modal
+          {/* close modal icon*/}
+          <div className="absolute z-30 right-2 top-2 hover:scale-110 duration-200 cursor-pointer">
+            <IoCloseOutline onClick={closeModal} className="text-4xl text-orange"/>
+          </div>
+
+          {/* pizza details */}
+          <PizzaDetails pizza={pizza} modal={modal} setModal={setModal}/>
         </Modal>
       )}
     </div>
--- client/src/app/components/PizzaDetails.js
+++ client/src/app/components/PizzaDetails.js
@@ -1,4 +1,38 @@
-const PizzaDetails = () => {
+import React, {useState, useEffect} from 'react'
+
+import Image from 'next/image'
+
+import SizeSelection from './SizeSelection';
+import CrustSelection from './CrustSelection';
+import Topping from './Topping';
+
+
+const PizzaDetails = ({pizza}) => {
+
+  const [size, setSize] = useState('small');
+  const [crust, setCrust] = useState('traditional');
+  const [additionalTopping, setAdditionalTopping] = useState([]);
+  const [additionalToppingPrice, setAdditionalToppingPrice] = useState(0);
+  const [price, setPrice] = useState(0);
+
+  useEffect(()=>{
+    size === 'small' ? setPrice(parseFloat(pizza.priceSm + additionalToppingPrice).toFixed(2)) : 
+    size === 'medium' ? setPrice(parseFloat(pizza.priceMd + additionalToppingPrice).toFixed(2)) : 
+    size === 'large' ? setPrice(parseFloat(pizza.priceLg + additionalToppingPrice).toFixed(2)) : 
+    null
+  })
+
+  useEffect(()=>{
+    if (additionalTopping.length>0){
+      const toppingPrice = additionalTopping.reduce((a,c)=> {
+        return a + c.price
+      }, 0)
+      setAdditionalToppingPrice(toppingPrice);
+    } else {
+      setAdditionalToppingPrice(0);
+    }
+  }, [additionalTopping]);
+
   return <div>PizzaDetails</div>;
 };
 
