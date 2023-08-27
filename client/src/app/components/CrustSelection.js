b9b6b2aaaf620b863dd5028e60493ba0d3653277
mamertmg <marquezgarcia.a@gmail.com>
2023-08-27 18:39:02 +0200

    aded crust and size selection

--- client/src/app/components/CrustSelection.js
+++ client/src/app/components/CrustSelection.js
@@ -1,5 +1,35 @@
-const CrustSelection = () => {
-  return <div>CrustSelection</div>;
+import Image from "next/image";
+
+const CrustSelection = ({crust, setCrust}) => {
+  return (
+    <div className="flex justify-center items-center lg:justify-start">
+      <div className="flex gap-x-12 mb-8 font-medium">
+        <label className="flex items-center gap-x-2 cursor-pointer">
+          <input 
+            type="radio" 
+            name="crust" 
+            value='traditional' 
+            checked={crust === 'traditional'}
+            onChange={(e)=>setCrust(e.target.value)}
+            className="appearance-none w-4 h-4 border rounded-full checked:bg-gradient-to-r checked:from-primary checked:to-secondary checked:border-secondary cursor-pointer"
+          />
+          Traditional
+        </label>
+        <label className="flex items-center gap-x-2 cursor-pointer">
+          <input 
+            type="radio" 
+            name="crust" 
+            value='thin' 
+            checked={crust === 'thin'}
+            onChange={(e)=>setCrust(e.target.value)}
+            className="appearance-none w-4 h-4 border rounded-full checked:bg-gradient-to-r checked:from-primary checked:to-secondary checked:border-secondary cursor-pointer"
+          />
+          Thin
+        </label>
+      </div>
+
+    </div>
+  );
 };
 
 export default CrustSelection;
--- client/src/app/components/PizzaDetails.js
+++ client/src/app/components/PizzaDetails.js
@@ -33,7 +33,66 @@ const PizzaDetails = ({pizza}) => {
     }
   }, [additionalTopping]);
 
-  return <div>PizzaDetails</div>;
+  return (
+    <div className='flex flex-col lg:flex-row lg:gap-x-8 h-full md:p-8'>
+      <div className='lg:flex-1 flex justify-center items-center'>
+        {/* pizza image */}
+        <div className='max-w-[300px] lg:max-w-none mt-6 lg:mt-0'>
+          <Image 
+            width={450} 
+            height={450} 
+            src={pizza.image} 
+            alt='' 
+            priority={1}
+            className='mx-auto relative'
+          />
+        </div>
+      </div>
+      {/* pizza details */}
+      <div className='flex flex-col flex-1'>
+        <div className='flex-1 p-2 text-center lg:text-left'>
+          <div className='flex-1 bg-white overflow-y-scroll h-[46vh] scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-white pr-2'>
+            {/* name */}
+            <div className='font-semibold'>
+              <h2 className='capitalize text-3xl mb-1'>{pizza.name}</h2>
+              <div className=' mb-6 text-lg font-medium'>
+                <span>
+                  {size === 'small' 
+                    ? '25 cm' 
+                    : size === 'medium' 
+                    ? '30 cm' 
+                    : size === 'large' 
+                    ? '35 cm' 
+                    : null
+                  }
+                </span>
+                <span>, {crust} crust </span>
+              </div>
+            </div>
+            {/* size selection */}
+            <SizeSelection pizza={pizza} size={size} setSize={setSize}/>
+            {/* crust selection */}
+            <CrustSelection crust={crust} setCrust={setCrust}/>
+            {/* toppings */}
+            <div className='mb-4 text-xl font-semibold'>Choose topping</div>
+            <div>
+              {pizza.toppings?.map((topping, index) => {
+                return <Topping key={index}/>;
+              })}
+            </div>
+          </div>
+        </div>
+        {/* add to cart btn */}
+        <div className='h-full flex items-center px-2 lg:items-end'>
+              <button className='btn btn-lg gradient w-full flex justify-center gap-x-2'>
+                <div>Add to cart for</div>
+                <div>$ {price}</div>
+                <div>Add to cart for</div>
+              </button>
+        </div>
+      </div>
+    </div>
+  )
 };
 
 export default PizzaDetails;
